a732c459821fede1ec34248aa247927c9d13c63f
zyixxx <zyixxx13@gmail.com>
2020-02-25 22:06:46 -0800

    added studio3, design comp1

---
+++ des157-wi20/studio3/script.js
@@ -0,0 +1,102 @@
+(function () {
+
+    "use strict";
+
+    const startGame = document.getElementById("startgame");
+    const gameControl = document.getElementById("gamecontrol");
+    const game = document.getElementById("game");
+    const score = document.getElementById("score");
+    const actionArea = document.getElementById("actions");
+
+    const gameData = {
+        dice: ["images/1die.jpg", "images/2die.jpg", "images/3die.jpg", "images/4die.jpg", "images/5die.jpg", "images/6die.jpg"],
+        players: ["player 1", "player 2"],
+        score: [0, 0],
+        roll1: 0,
+        roll2: 0,
+        rollSum: 0,
+        index: 0,
+        gameEnd: 29
+    }
+
+    startGame.addEventListener("click", function () {
+        gameData.index = Math.round(Math.random());
+        gameControl.innerHTML = '<h2>The Game Has Started</h2>';
+        gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
+
+        document.getElementById('quit').addEventListener("click", function () {
+            location.reload();
+        });
+
+        setUpTurn();
+    });
+
+    function setUpTurn() {
+        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
+        document.getElementById('roll').addEventListener("click", function () {
+            throwDice();
+        });
+        checkWinningCondition();
+    }
+
+    function throwDice() {
+        actionArea.innerHTML = '';
+        gameData.roll1 = Math.ceil(Math.random() * 6);
+        gameData.roll2 = Math.ceil(Math.random() * 6);
+        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        game.innerHTML += `<img class="dice" src="${gameData.dice[gameData.roll1 - 1]}" width="100px" height="110px"> 
+						<img class="dice" src="${gameData.dice[gameData.roll2 - 1]}" width="100px" height="110px">`;
+        gameData.rollSum = gameData.roll1 + gameData.roll2;
+
+        // if two 1's are rolled...
+        if (gameData.rollSum === 2) {
+            console.log("snake eyes were rolled");
+            game.innerHTML += '<p>Oh snap! Snake eyes!</p>';
+            gameData.score[gameData.index] = 0;
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            showCurrentScore();
+            setTimeout(setUpTurn, 2000);
+        }
+        // if either die is a 1...
+        else if (gameData.roll1 === 1 || gameData.roll2 === 1) {
+            console.log("one of the two dice was a 1");
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</p>`;
+            setTimeout(setUpTurn, 2000);
+        }
+        // if neither die is a 1...
+        else {
+            console.log("the game proceeds");
+            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
+            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
+
+            document.getElementById('rollagain').addEventListener("click", function () {
+                throwDice();
+            });
+
+            document.getElementById('pass').addEventListener("click", function () {
+                gameData.index ? gameData.index = 0 : gameData.index = 1;
+                setUpTurn();
+            });
+
+            checkWinningCondition();
+        }
+    }
+
+    function checkWinningCondition() {
+        if (gameData.score[gameData.index] > gameData.gameEnd) {
+            score.innerHTML = `<h2>${gameData.players[gameData.index]} 
+		wins with ${gameData.score[gameData.index]} points!</h2>`;
+
+            actionArea.innerHTML = '';
+            document.getElementById('quit').innerHTML = "Start a New Game?";
+        }
+        else {
+            showCurrentScore();
+        }
+    }
+    function showCurrentScore() {
+        score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]}: ${gameData.score[0]}</strong> and <strong>${gameData.players[1]}: ${gameData.score[1]}</p>`;
+    }
+}());
